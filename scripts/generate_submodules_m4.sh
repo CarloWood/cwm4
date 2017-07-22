9feb6702973aedb976af6db459711cb402ea6a08
Carlo Wood <carlo@alinoe.com>
2017-07-22 16:55:27 +0200

    Automatic commit of changes by autogen.sh.

--- scripts/generate_submodules_m4.sh
+++ scripts/generate_submodules_m4.sh
@@ -49,7 +49,7 @@ AC_SUBST([CW_SUBDIRS], [""])
 #AM_SUBST_NOTMAKE([CW_SUBDIRS])
 EOF
   fi
-elif test -f "$1/configure.m4"; then
+elif test -f "configure.m4"; then
   # Script is called from git submodule foreach ...'
   submodule_path="$1"
   submodule_dirname="$(dirname "$submodule_path")"
