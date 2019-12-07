97018156b04a4715649385863e11c5113f8de25a
Carlo Wood <carlo@alinoe.com>
2019-12-07 23:46:12 +0100

    Make autotools work while using a 'cmake' config.h.in.

---
+++ scripts/convert_config_h.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+srcdir="$(echo $0 | sed -e 's%/cwm4/scripts/convert_config_h\.sh%%')"
+
+for f in $config_files; do
+  if [ $(basename $f) = "config.h" ]; then
+    echo "configure: creating $f.in"
+    awk  '/^#cmakedefine01/ { next; }
+          /^#cmakedefine/ { $0 = gensub(/^#cmakedefine\s+([^ ]+).*/, "#@CW_CONFIG_\\1@ \\1", "g"); print; next; }
+          { print; }' $srcdir/$f.in > $f.in
+  fi
+done
