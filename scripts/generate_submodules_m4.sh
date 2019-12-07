c1df88cd43c5e2de496aacaa73ba599d59f1c442
Carlo Wood <carlo@alinoe.com>
2019-12-07 22:09:52 +0100

    Automatic commit of changes by autogen.sh.

--- scripts/generate_submodules_m4.sh
+++ scripts/generate_submodules_m4.sh
@@ -52,7 +52,7 @@ if test "$opt_foreach" -eq 0; then
 AC_SUBST([CW_SUBDIRS], "CW_SUBMODULE_SUBDIRS")
 #AM_SUBST_NOTMAKE([CW_SUBDIRS])
 AC_CONFIG_FILES(CW_SUBMODULE_CONFIG_FILES)
-AC_CONFIG_FILES([], [], [set | grep 'config\.h'; echo XXX\${config_files}YYY; \${srcdir}/cwm4/scripts/convert_config_h.sh \${config_headers}])
+AC_CONFIG_FILES([], [], [set | grep 'config\.h'; eval echo XXX\${config_files}YYY; \${srcdir}/cwm4/scripts/convert_config_h.sh \${config_headers}])
 EOF
   else
     cat << EOF >> m4/submodules.m4
