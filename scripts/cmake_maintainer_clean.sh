622d3380d062e05c1cb37ca20a00b2df673d3224
Carlo Wood <carlo@alinoe.com>
2019-12-11 23:34:51 +0100

    Require include(cwm4/cmake/AICxxProject) in root CMakeLists.txt. Add target maintainer-clean

---
+++ scripts/cmake_maintainer_clean.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+MAKE="$1"
+
+if [ ! -x "$MAKE" ]; then
+  echo "Unexpected error: \"$MAKE\" is not executable."
+  exit 1
+fi
+
+echo "Running 'make clean' ..."
+"$MAKE" --no-print-directory clean
+
+# This doesn't work for paths containing new-lines...
+dirs=(.)
+readarray -t subdirs < <(find . -type d -printf "%P\n")
+dirs+=("${subdirs[@]}")
+
+for dir in "${subdirs[@]}"; do
+  if [ -e "$dir"/Makefile ]; then
+    base=$(dirname "$dir")
+    name=$(basename "$dir")
+    if $MAKE -C "$base" -n "maintainer-clean-$name" >/dev/null 2>/dev/null; then
+      $MAKE -C "$base" "maintainer-clean-$name"
+    fi
+  fi
+done
+
+for dir in "${dirs[@]}"; do
+  if [ -e "$dir"/Makefile ]; then
+    echo "Maintainer cleaning $dir"
+    rm -rf "$dir"/CMakeFiles "$dir"/CMakeCache.txt "$dir"/cmake_install.cmake "$dir"/Makefile
+    # Should be entirely empty now.
+    rmdir $dir
+  fi
+done
