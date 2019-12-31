0760d5e8177110629f5875fefa24b22e8ac67429
Carlo Wood <carlo@alinoe.com>
2019-12-31 11:40:32 +0100

    W.I.P.

--- cmake/HunterGate.cmake
+++ cmake/HunterGate.cmake
@@ -1,7 +1,23 @@
-# Note: use -DHUNTER_ENABLED=NO to turn off the usage of Hunter.
-include( cwm4/gate/cmake/HunterGate.cmake )
+# FetchContent_MakeAvailable is available since CMake version 3.14.
+cmake_minimum_required( VERSION 3.14 )
 
-# Hunter Gate to use when Hunter is turned on.
+include( FetchContent )
+
+# Uncomment to use local git submodule for Hunter.
+#set( HUNTER_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/cwm4/hunter" )
+
+# Setting up external packages.
+#set( HUNTER_Boost_COMPONENTS filesystem iostreams program_options system )
+#set( HUNTER_PACKAGES Boost farmhash )
+set( HUNTER_PACKAGES farmhash )
+
+# Use local git submodule in the source tree.
+FetchContent_Declare( SetupHunter SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cwm4/hunter/gate" )
+# Initialize Hunter and build external pacakges.
+FetchContent_MakeAvailable( SetupHunter )
+
+# Refresh CMAKE_MODULE_PATH (this call isn't using the URL or SHA1, what is really being
+# used can be found in the generated ${CMAKE_CURRENT_BINARY_DIR}/HunterGate.cmake.
 HunterGate(
     URL "https://github.com/cpp-pm/hunter/archive/v0.23.240.tar.gz"
     SHA1 "ca19f3769e6c80cfdd19d8b12ba5102c27b074e0"
