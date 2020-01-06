977ce6a746244accfb7577bcc45926364adfe9b7
Carlo Wood <carlo@alinoe.com>
2020-01-06 17:38:47 +0100

    Bug fix: create directory of output config.h.in if not exist.

--- scripts/convert_config_h.sh
+++ scripts/convert_config_h.sh
@@ -5,6 +5,7 @@ srcdir="$(echo $0 | sed -e 's%/cwm4/scripts/convert_config_h\.sh%%')"
 for f in $config_files; do
   if [ $(basename $f) = "config.h" ]; then
     echo "configure: creating $f.in"
+    mkdir -p "$(dirname $f)"
     awk  '/^#cmakedefine01/ { next; }
           /^#cmakedefine/ { $0 = gensub(/^#cmakedefine\s+([^ ]+).*/, "#@CW_CONFIG_\\1@ \\1", "g"); print; next; }
           { print; }' $srcdir/$f.in > $f.in
