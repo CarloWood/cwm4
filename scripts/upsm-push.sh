cc10f9df28a47202e53233179742112b21201613
Carlo Wood <carlo@alinoe.com>
2020-01-12 19:02:51 +0100

    Commit gitlink changes.

--- scripts/upsm-push.sh
+++ scripts/upsm-push.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if ! git submodule --quiet foreach "$(realpath cwm4/scripts/update_submodule.sh)"' --quiet $name "$path" $sha1 "$toplevel"'; then
+if ! git submodule --quiet foreach "$(realpath cwm4/scripts/update_submodule.sh)"' --quiet --commit $name "$path" $sha1 "$toplevel"'; then
   echo >&2 "cwm4/scripts/upsm-push.sh: fatal error."
   exit 1
 fi
