0a62e5f4b0cbb49fc3738f4faea7c8cb9d9e31a5
Carlo Wood <carlo@alinoe.com>
2023-12-12 18:25:01 +0100

    Automatic commit of changes by autogen.sh.

--- cmake/AICxxSubmodules.cmake
+++ cmake/AICxxSubmodules.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 
 # This is a list of all aicxx submodules that exist.
 # Submodules on the right depend on the submodules on the left (but not on events or xml).
-set(AICxxSubmodules cwds utils xml events threadsafe threadpool evio statefultask fastprimes)
+set(AICxxSubmodules cwds utils xml events threadsafe threadpool evio statefultask fastprimes cairowindow)
 
 foreach (subdir ${AICxxSubmodules})
   get_filename_component(_fullpath "${subdir}" REALPATH)
